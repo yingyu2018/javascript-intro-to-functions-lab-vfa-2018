90fde9829c6e83d103cd849bc65979cbf37d1368
Yingying Yu <yingying_yu@alumni.brown.edu>
2018-06-29 20:05:09 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,64 @@
+function shout(string) {
+  return string.toUpperCase()
+}
+describe('shout(string)', function() {
+  it('receives one argument and returns it in all caps', function() {
+    expect(shout('hello')).toEqual('HELLO')
+  })
+})
+function whisper(string) {
+  return string.toLowerCase()
+}
+describe('whisper(string)', function() {
+  it('receives one argument and returns it in all lowercase', function() {
+    expect(whisper('HELLO')).toEqual('hello')
+  })
+})
+function logShout(string) {
+  return console.log().toUpperCase
+}
+describe('logShout(string)', function() {
+  it('calls console.log() its one argument in all caps', function() {
+    const spy = expect.spyOn(console, 'log').andCallThrough()
+
+    logShout('hello')
+
+    expect(spy).toHaveBeenCalledWith('HELLO')
+
+    console.log.restore()
+  })
+})
+function logWhisper(string) {
+  return console.log().toLowerCase
+}
+describe('logWhisper(string)', function() {
+  it('calls console.log() its one argument in all lowercase', function() {
+    const spy = expect.spyOn(console, 'log').andCallThrough()
+
+    logWhisper('HELLO')
+
+    expect(spy).toHaveBeenCalledWith('hello')
+
+    console.log.restore()
+  })
+})
+function sayHiToGrandma(lowercase) {
+  return "I can't hear you!"
+}
+describe('sayHiToGrandma(string)', function() {
+  it('returns "I can\'t hear you!" if `string` is lowercase', function() {
+    expect(sayHiToGrandma('hello')).toEqual("I can't hear you!")
+  })
+function sayHiToGrandma(Uppercase) {
+  return "YES INDEED!"
+}
+  it('returns "YES INDEED!" if `string` is uppercase', function() {
+    expect(sayHiToGrandma('HELLO')).toEqual("YES INDEED!")
+  })
+function sayloveToGrandma(string) {
+  return "I love you, too.";
+}
+  it('returns "I love you, too." if `string` is "I love you, Grandma."`', function() {
+    expect(sayHiToGrandma("I love you, Grandma.")).toEqual("I love you, too.")
+  })
+})
