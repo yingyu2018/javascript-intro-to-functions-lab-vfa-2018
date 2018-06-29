ba1439446403e602109ca7a0b9abf62f5da5f89b
Yingying Yu <yingying_yu@alumni.brown.edu>
2018-06-29 20:08:02 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ describe('whisper(string)', function() {
   })
 })
 function logShout(string) {
-  console.log() string.toUpperCase
+  console.log(string.toUpperCase) 
 }
 describe('logShout(string)', function() {
   it('calls console.log() its one argument in all caps', function() {
